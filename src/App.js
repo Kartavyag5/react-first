15119ba1add72506bb5f14e0091481c20e8a5f77
Kartavyag5 <Kartavyag5@gmail.com>
2022-01-16 18:01:37 +0530

    update: use axios instead of fetch()

--- src/App.js
+++ src/App.js
@@ -1,40 +1,29 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
 import './App.css';
 
-class App extends React.Component {
 
-  constructor(){
-    super();
-    this.state = {
-      users:null
-    }
-  }
+export default function App() {
+  const [users, setUsers] = React.useState(null);
 
-  componentDidMount(){
-    fetch('https://reqres.in/api/users').then((resp)=>{
-      resp.json().then(result=>{
-        console.warn(result.data)
-        this.setState({
-          users:result.data
-        })
-      })
-    })
-  }
-  render() {
-    return(
-      <div className="App">
-        <h1>Fetch API Data</h1>
-        {
-          this.state.users ?
-          this.state.users.map((item,i)=>
-          <div><p>{i}--{item.first_name} {item.last_name}</p></div>
-          )
-          :
-          null
-        }
-      </div>
-    );
-  }
+  React.useEffect(() => {
+    axios.get("https://reqres.in/api/users").then((response) => {
+      setUsers(response.data);
+      console.log(response.data)
+    });
+  }, []);
+
+  if (!users) return null;
+  console.log(users.data[0].first_name);
+
+  return (
+    <div>
+      <h1>Get API Example</h1>
+      {users.data? users.data.map((item,i)=>
+          <p>{i}--{item.first_name} {item.id}</p>
+      )
+      :
+      null }
+    </div>
+  );
 }
-
-export default App;
